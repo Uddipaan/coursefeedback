ab1ceacf4aba8c8ab6feeffa7781aca2606e18a2
uddipaan <uddipaanh369@gmail.com>
2020-09-13 11:33:06 +0530

    initial commit

---
+++ src/main/java/com/Parag/coursefeedback/CoursefeedbackApplication.java
@@ -0,0 +1,13 @@
+package com.Parag.coursefeedback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoursefeedbackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoursefeedbackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Parag/coursefeedback/CoursefeedbackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Parag.coursefeedback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoursefeedbackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
